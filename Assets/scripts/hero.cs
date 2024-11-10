0d1316382049fa6aa46562cadb5cfb1c1ca0bada
Ibukhvalov <iabukhvalov@edu.hse.ru>
2024-11-10 17:55:13 +0300

    added animations

--- Assets/scripts/hero.cs
+++ Assets/scripts/hero.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
- 
+
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float runningSpeed;
     [SerializeField] private float jumpSpeed;
     [SerializeField] private int jumpCount;
+
+    [SerializeField] private bool isGrounded;
+    
     private Rigidbody2D body;
     private SpriteRenderer sprite;
+    private Animator animator;
     
+    
+     
     private bool isTouchSurface;
 
     private void Awake()
@@ -15,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
         //Grabs references for rigidbody and animator from game object.
         body = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponentInChildren<Animator>();
         runningSpeed = 3;
         jumpSpeed = 6;
         jumpCount = 2;
@@ -30,27 +37,41 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump();
         }
-        
 
-        //sets animation parameters
+        UpdateAnimator();
+        isGrounded = isOnGround();
         
     }
  
     private void Jump()
     {
+        animator.SetTrigger("jump");
         body.velocity = new Vector2(body.velocity.x, jumpSpeed);
         jumpCount--;
         isTouchSurface = false;
     }
-    
 
- 
+    private void UpdateAnimator()
+    {
+        animator.SetBool("run" , Input.GetAxis("Horizontal") != 0);
+        animator.SetBool("grounded", isOnGround());
+        if (body.velocity.y < 0)
+            animator.SetTrigger("drop");
+        if (body.velocity.magnitude > 0.01)
+            transform.localScale = body.velocity.x > 0 ? Vector3.one : new Vector3(-1,1,1);
+    }
+
+
+    private bool isOnGround()
+    {
+        Collider2D[] col = Physics2D.OverlapCircleAll(transform.position, 0.1f);
+        return col.Length > 1;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Ground"))
         {
-            Collider2D[] col = Physics2D.OverlapCircleAll(transform.position, 0.1f);
-            bool isGrounded = col.Length > 1;
+            bool isGrounded = isOnGround();
             jumpCount = isGrounded ? 2 : 1;
             isTouchSurface = true;
         }
